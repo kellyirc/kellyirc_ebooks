6328a0670533fcdfac81a0f32e5f65a732945ee2
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-02-23 23:22:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# kellyirc_ebooks
+
+The twitter_ebooks bot for #kellyirc.
+
+## Setup
+
+I have no clue how ruby works? Clone this repository, then:
+
+```sh
+bundle install
+ruby build.rb
+ebooks start
+```
+
+Maybe. That might work. Iunno.
---
+++ build.rb
@@ -0,0 +1,15 @@
+# Builds the model by retrieving the last ~3.2k tweets of our twitters
+# Run this before starting `ebooks start`
+
+`rm -r model corpus`
+`mkdir model corpus`
+
+KELLYIRC_USERS = ["rahatarmanahmed", "rayyukay", "seiyria", "spookyco"]
+
+KELLYIRC_USERS.each do |user|
+    `ebooks archive #{user} corpus/#{user}.json`
+end
+
+CORPUS_PATHS = KELLYIRC_USERS.map { |user| "corpus/#{user}.json" }
+
+`ebooks consume-all all #{CORPUS_PATHS.join ' '}`
